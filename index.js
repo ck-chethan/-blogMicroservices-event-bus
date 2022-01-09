50af7e5c85797b06cbdece50734160e05ca32791
chethanck <chethanckm66@gmail.com>
2022-01-09 13:36:28 +0530

    event-bus service changes

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-const express = require("express");
-const axios = require("axios");
+const express = require('express');
+const axios = require('axios');
 
 const app = express();
 
 app.use(express.json());
 
 app.post('/events', (req, res) => {
-    const event = req.body;
+  const event = req.body;
 
-    axios.post('http://localhost:4000/events', event).catch(err => console.log(err.message));
-    axios.post('http://localhost:4001/events', event).catch(err => console.log(err.message));
-    axios.post('http://localhost:4002/events', event).catch(err => console.log(err.message));
+  axios
+    .post('http://localhost:4000/events', event)
+    .catch((err) => console.log(err.message));
+  axios
+    .post('http://localhost:4001/events', event)
+    .catch((err) => console.log(err.message));
+  axios
+    .post('http://localhost:4002/events', event)
+    .catch((err) => console.log(err.message));
+  axios
+    .post('http://localhost:4003/events', event)
+    .catch((err) => console.log(err.message));
 
-    res.send({ status: 'ok' });
+  res.send({ status: 'ok' });
 });
 
-
 app.listen(4005, () => {
-    console.log('Event server started on port 4005');
+  console.log('Event server started on port 4005');
 });
